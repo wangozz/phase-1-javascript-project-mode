36b8542fdb7b8daab96c49d1c511b58000b88a15
SamuelWango <wangos478@gmail.com>
2023-07-06 21:23:09 +0300

    almost done

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-document.getElementById("contact").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default link behavior
-  
-    // Display an alert with the phone number
-    alert("Please call us at 0712345678 for assistance.");
-  });
-  
+    // Fetch the JSON file
+    fetch('diary.json')
+      .then(response => response.json()) // Parse the response as JSON
+      .then(data => {
+        // Access the diary entries
+        const diaryEntries = data.diaryEntries;
+
+        // Get the 'entries' element
+        const entriesContainer = document.getElementById('diary');
+
+        // Iterate through the diary entries
+        diaryEntries.forEach(entry => {
+          // Create a new paragraph element for each entry
+          const entryElement = document.createElement('p');
+          entryElement.textContent = `${entry.date}: ${entry.entry}`;
+
+          // Append the entry element to the 'entries' container
+          entriesContainer.appendChild(entryElement);
+        });
+      })
+      .catch(error => {
+        console.log('An error occurred:', error);
+      });
+
+      
