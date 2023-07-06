486d4465283c78e954e9fa21e0f5f219582cf33b
SamuelWango <wangos478@gmail.com>
2023-07-06 14:47:59 +0300

    design done

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contact").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+  
+    // Display an alert with the phone number
+    alert("Please call us at 0712345678 for assistance.");
+  });
+  
